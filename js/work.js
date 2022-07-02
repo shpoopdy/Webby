baeb9d43431777f20238bcf54ead2b0e6b72d4e8
Mikey <shpoopdy@gmail.com>
2022-07-02 12:30:08 -0700

    Create work.js

--- js/work.js
+++ js/work.js
@@ -1,15 +1,46 @@
-const RANDOM_QUOTE_API = 'http://api.quotable.io/random'
-const quoteDisplay = document.getElementById('')
+const typingText = document.querySelector(".typing-text p"),
+inputField = document.querySelector(".wrapper .input-field");
+mistakeTag = document.querySelector(".mistake span");
 
-function getQuote() {
-  return fetch(RANDOM_QUOTE_API)
-    .then(response => response.json())
-    .then(data => data.content);
+let charIndex = 0;
+let mistakesMade = 0;
+
+function randomParagraph() {
+  let randomIndex = Math.floor(Math.random() * paragraphs.length);
+  paragraphs[randomIndex].split("").forEach(span => {
+    let spanTag = `<span>${span}</span>`;
+    typingText.innerHTML += spanTag;
+  });
+  document.addEventListener("keydown", () => inputField.focus());
+  typingText.addEventListener("click", () => inputField.focus());
 }
 
-async function getNextQuote() {
-  const quote = await getQuote();
+function initTyping(event) {
+  const characters = typingText.querySelectorAll("span");
+  let typedChar = inputField.value.split("")[charIndex];
 
+  if(typedChar == null) {
+    charIndex--;
+    if(characters[charIndex].classList.contains("incorrect")) {
+      mistakesMade--;
+    }
+    characters[charIndex].classList.remove("correct", "incorrect");
+  }
+  else {
+    if(characters[charIndex].innerText === typedChar) {
+      //adds correct to span class if correct, incorrect otherwise.
+      characters[charIndex].classList.add("correct");
+    }
+    else {
+      mistakesMade++;
+      characters[charIndex].classList.add("incorrect");
+    }
+    charIndex++;
+  }
+  mistakeTag.innerText = mistakesMade;
+
+  //characters[charIndex].classList.add("active");
 }
 
-getQuote();
+randomParagraph();
+inputField.addEventListener("input", initTyping);
