de15ad902a0566eebe168522bd73b48b43ed7bb7
Mikey <shpoopdy@gmail.com>
2022-07-02 12:30:24 -0700

    Create paragraph.js

---
+++ js/paragraphs.js
@@ -0,0 +1,6 @@
+const paragraphs = [
+  "Any attempt at recovering the bodies was absolutely hopeless, and there, deep down in that dreadful caldron of swirling water and seething foam, will lie for all time the most dangerous criminal and the foremost champion of the law of their generation.",
+  "No matter how hard you try to be what you once were, you can only be what you are here and now.",
+  "Phases of her childhood lurked in her aspect still. As she walked along to-day, for all her bouncing handsome womanliness, you could sometimes see her twelfth year in her cheeks, or her ninth sparkling from her eyes; and even her fifth would flit over the curves of her mouth now and then.",
+  "Men's courses will foreshadow certain ends, to which, if persevered in, they must lead,\" said Scrooge. \"But if the courses be departed from, the ends will change. Say it is thus with what you show me!",
+]
