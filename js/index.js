ccfc9488d8c840bfe5b28982119f28c8c7f27e37
Mike <shpoopdy@gmail.com>
2022-10-26 21:08:34 -0700

    Add functionality to switch tag

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 document.getElementById('time').addEventListener("mouseover", englishGreet);
 document.getElementById('time').addEventListener("mouseleave", displayGreeting);
+document.getElementById('theme').addEventListener("click", themeSwitch);
 let timey = document.getElementById('time');
 
 function englishGreet() {
@@ -36,6 +37,44 @@ function displayGreeting() {
     }
 
     timey.innerHTML = greeting;
+}
+
+function themeSwitch() {
+    let theme = document.getElementById('theme');
+    let pList = document.querySelectorAll('p');
+    let h1List = document.querySelectorAll('h1');
+    let h3List = document.querySelectorAll('h3');
+    let liList = document.querySelectorAll('a');
+    if (theme.checked == true) {
+        document.body.style.background = '#E7DEA8';
+        for (let i = 0; i <pList.length; i++) {
+            pList[i].style.color = 'black';
+        }
+        for (let j = 0; j < h1List.length; j++) {
+            h1List[j].style.color = 'black';
+        }
+        for (let k = 0; k < h3List.length; k++) {
+            h3List[k].style.color = 'black';
+        }
+        for (let l = 0; l < liList.length; l++) {
+            liList[l].style.color = 'black';
+        }
+        //document.getElementById('body-text').style.color = 'black';
+    } else {
+        document.body.style.background = '#0B1117';
+        for (let i = 0; i < pList.length; i++) {
+            pList[i].style.color = 'white';
+        }
+        for (let j = 0; j < h1List.length; j++) {
+            h1List[j].style.color = 'white';
+        }
+        for (let k = 0; k < h3List.length; k++) {
+            h3List[k].style.color = 'white';
+        }
+        for (let l = 0; l < liList.length; l++) {
+            liList[l].style.color = 'white';
+        }
+    }
 }
 
 displayGreeting();
