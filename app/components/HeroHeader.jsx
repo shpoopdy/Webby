bceb981831eb08dd37e99bd874773d124b4ef4bb
Mikey <shpoopdy@gmail.com>
2023-10-28 16:49:19 -0700

    Add more website info

--- app/components/HeroHeader.jsx
+++ app/components/HeroHeader.jsx
@@ -1,11 +1,17 @@
 import Image from 'next/image';
+import styles from './HeroHeader.module.css';
 
 export default function HeroHeader() {
     return (
-        <div>
-            <h1>Mikey</h1>
-            <p>10:00am</p>
-            <Image src="/images/me.png" alt="Mikey's Image" width={100} height={100}/>
+        <div className={styles.heroContain}>
+            <div>
+              <Image src="/images/me.png" alt="Mikey's Image" width={250} height={250} className={styles.hero_image} />
+            </div>
+        
+            <div className={styles.hero_feature}>
+              <h1>Mikey</h1>
+              <p>10:00am</p>
+            </div>
         </div>
     )
 }
--- app/page.jsx
+++ app/page.jsx
@@ -3,7 +3,16 @@ import styles from './page.module.css'
 export default function Home() {
   return (
     <main>
-      <h1>Hi</h1>
+      <div className={styles.mainContain}>
+        <h3>About</h3>
+        <p>'Hello! My name is Michael and I currently have a B.S. degree in Computer Science. I enjoy facing new challenges and diving into the deep end to start figuring things out.</p>
+          <br></br>
+        <p>
+          I have done projects using languages like C++, C#, JavaScript, Python, and PHP. I have also done some personal projects outside of class that have allowed me to gain a deeper understanding of what I\'ve been learning, most of which can be viewed on my GitHub page. In my free time I like to play chess, piano, study the violin, play video games, make YouTube videos, go hiking, and just socialize with people.'
+        </p>
+
+        <h3>Projects</h3>
+      </div>
     </main>
   )
 }
