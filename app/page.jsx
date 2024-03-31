fb22be8610c8501d28dca0aaa5a6664f73a2fee3
shpoopdy <shpoopdy@gmail.com>
2024-03-31 15:22:54 -0700

    Finish updating about

--- app/page.jsx
+++ app/page.jsx
@@ -20,7 +20,13 @@ export default function Home() {
         <h3>About</h3>
         <p>Hello! My name is Michael and I currently work as a programmer for <a className='p_links' href='https://www.humboldt.edu' target='_blank'>California State Polytechnic University, Humboldt</a></p>&nbsp; 
  
-        <p>Some more text dog</p>
+        <p>I have a passion for helping people if I can and a passion for technology. I've been blessed to be able to have a job that enables me
+        to have the best of both worlds. When I'm not at work, but I'm still feeling in the zone I like to work on my personal website, this one,
+        as well as some ideas that I have floating around in my head that will be of use to everyone.
+        </p>
+
+        <h3>I Love</h3>
+        <p>Nature, friends, the ocean, making music, making YouTube videos, and being cozy.</p>
   
         <h3>Projects</h3>
         <div>
