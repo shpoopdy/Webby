4794669ff213d1b0028adfaa5eca99d1422e4f5d
Mikey <shpoopdy@gmail.com>
2024-08-15 12:04:55 -0700

    Update icons and readme with attributes.

--- README.md
+++ README.md
@@ -4,3 +4,6 @@ My current website done using Next.js & React!
 # Icons I Did Not Create
 [moon](<a href="https://www.flaticon.com/free-icons/moon" title="moon icons">Moon icons created by Freepik - Flaticon</a>)
 [sun](<a href="https://www.flaticon.com/free-icons/sun" title="sun icons">Sun icons created by Good Ware - Flaticon</a>)
+[linkedin](<a href="https://www.flaticon.com/free-icons/linkedin" title="linkedin icons">Linkedin icons created by riajulislam - Flaticon</a>)
+[instagram](<a href="https://www.flaticon.com/free-icons/instagram" title="instagram icons">Instagram icons created by edt.im - Flaticon</a>)
+[youtube](<a href="https://www.flaticon.com/free-icons/youtube" title="youtube icons">Youtube icons created by Freepik - Flaticon</a>)
--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -1,18 +1,36 @@
+import { useContext } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 import styles from './Footer.module.css';
+import { ThemeContext } from '../layout';
 
 export default function Footer() {
+    const { theme } = useContext(ThemeContext);
     return (
         <footer className={styles.footer}>
             <Link href="https://www.youtube.com/channel/UCaRbyJwt7N2RtOB7tWPPN3w">
-                <Image src="/images/tubes.png" alt="YouTube Image" width={20} height={20}></Image>
+                <Image
+                    src={theme === 'light' ? "/images/youtube.png" : "/images/youtube-dark.png"}
+                    alt="YouTube Icon"
+                    width={20}
+                    height={20}>
+                </Image>
             </Link>
             <Link href="https://www.instagram.com/shpoopdy">
-                <Image src="/images/insta.png" alt="Instagram Image" width={20} height={20}></Image>
+                <Image
+                    src={theme === 'light' ? "/images/instagram.png" : "/images/instagram-dark.png"}
+                    alt="Instagram Icon"
+                    width={20}
+                    height={20}>
+                </Image>
             </Link>
             <Link href="https://www.linkedin.com/in/michael-c-b00a15104/">
-                <Image src="/images/linked.png" alt="LinkedIn Image" width={20} height={20}></Image>
+                <Image
+                    src={theme === 'light' ? "/images/linkedin.png" : "/images/linkedin-dark.png"}
+                    alt="Linkedin Icon"
+                    width={20} 
+                    height={20}>
+                </Image>
             </Link>
         </footer>
     )
