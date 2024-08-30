c0f998c9f3c23f87bddc888c2892ab6584926ffb
Mikey <shpoopdy@gmail.com>
2024-08-30 13:41:06 -0700

    Create Navbar to be parent container.

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,22 +1,23 @@
 import { useContext } from 'react';
 import { ThemeContext } from '../layout';
-import Link from 'next/link';
 import Image from 'next/image';
-import NavLinks from './NavLinks';
+import DesktopNavbar from './DesktopNavbar';
+import MobileNavbar from './MobileNavbar';
 
 
 export default function Navbar() {
   const { theme, toggleTheme } = useContext(ThemeContext);
   return (
     <>
-      <nav className='DesktopNavbar'>
+      <nav>
         <button
           className='theme-toggle'
           id={theme === 'light' ? 'dark' : 'light'}
           onClick={() => {toggleTheme(theme === 'light' ? 'dark' : 'light')}}>
           <Image id='btn-img' src={theme === 'light' ? "/images/moon.png" : "/images/sun.png"} alt="Moon Icon" width={25} height={25} />
         </button>
-        <NavLinks />
+        <DesktopNavbar />
+        <MobileNavbar />
       </nav>
     </>
   )
